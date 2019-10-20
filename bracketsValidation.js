cd462eb99ce178dfabbf06f80cdec26d3ce03b1d
Gumer Shagiyanov <>
2019-10-20 16:42:30 +0300

    Add brackets validation

---
+++ bracketsValidation.js
@@ -0,0 +1,25 @@
+window.validateBrackets = function (valueToValidate) {
+    let bracketsStack = [];
+    let isValid = true;
+
+    valueToValidate.split('').forEach(value => {
+        if (value === '{' || value === '(' || value === '[') {
+            bracketsStack.push(value);
+        }
+        if (value === '}' &&  bracketsStack.pop() !== '{') {
+            isValid = false;
+        }
+        if (value === ']' &&  bracketsStack.pop() !== '[') {
+            isValid = false;
+        }
+        if (value === ')' &&  bracketsStack.pop() !== '(') {
+            isValid = false;
+        }
+
+    });
+
+    return isValid;
+
+};
+
+console.log(validateBrackets('{qwe[sdf(sdf)(ewer)sdf]sdf}'));
